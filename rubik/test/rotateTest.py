94cf3771a0c72585ba0d75c71fbcc6855cdea44b
Jonathan <jhs0051@auburn.edu>
2023-01-30 20:20:03 -0600

    Green Light    |

--- rubik/test/rotateTest.py
+++ rubik/test/rotateTest.py
@@ -13,3 +13,18 @@ class RotateTest(TestCase):
             
         actualResult = rotate(parms)
         self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
+
+    def test_rotate_020_ShouldRotateCubeWithExtraneousKey(self):
+        parms = {}
+        parms['op'] = 'rotate'
+        parms['extraneous'] = 'key'
+        parms['cube'] = 'ogwwrywybgyrgbgrrwoogbgwyrworyryggwbbbyyowgobroobwoybr'
+        parms['dir'] = 'F'
+        
+        expectedResult = {}
+        expectedResult['cube'] = 'wwoyrgbywgyrobgbrwoogbgwyrworrryogwobbyyowbgyrggbwoybr'
+        expectedResult['status'] = 'ok'
+
+        actualResult = rotate(parms)
+        self.assertEqual(expectedResult.get('cube'), actualResult.get('cube')) 
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
