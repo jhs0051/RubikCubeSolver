9225d16818974dd4db9c76c80883f77722e947ac
Jonathan <jhs0051@auburn.edu>
2023-02-02 22:53:17 -0600

    Green Light    |

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -47,7 +47,7 @@ class Cube:
             return True
         
     def rotate(self, directions):
-        if directions == '' or None:
+        if directions == '' or directions == None:
             self._rotate_F()
         else:
             for rotationDirection in directions:
