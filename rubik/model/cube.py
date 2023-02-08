eda1e09fdee35fcc447864b71bd147af3c965330
Jonathan <jhs0051@auburn.edu>
2023-02-08 17:37:00 -0600

    fix failing iteration 1 test

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -16,7 +16,7 @@ class Cube:
     def _validation(encodedCube, directions):
         cube = encodedCube
         validDirections = 'F,f,R,r,B,b,L,l,U,u'
-        validCharacters = 'Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,v,b,n,m,1,2,3,4,5,6,7,8,9,0'
+        validCharacters = 'Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,1,2,3,4,5,6,7,8,9,0'
         middleColorsList = list()
         
         if len(cube) != 54:
