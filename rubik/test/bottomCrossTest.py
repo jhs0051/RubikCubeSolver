18215b576241917e473a4028ada79330411e810a
Jonathan <jhs0051@auburn.edu>
2023-02-16 20:09:59 -0600

    Red Light      |

--- rubik/test/bottomCrossTest.py
+++ rubik/test/bottomCrossTest.py
@@ -64,6 +64,19 @@ class BottomCrossTest(unittest.TestCase):
         self.assertEqual(expectedRotations, actualResult.get('solution'))
         self.assertEqual(expectResult.get('status'), actualResult.get('status'))
         
+    def test_bottomCross_050_MakesBottomCrossWithStatusOk(self):
+        parms = {}
+        parms['op'] = 'solve'
+        parms['cube'] = 'gwgywyyrrooroywyyrwogbgrwwwryobbwbbobgbgobbrywgggrrooy'
+
+        expectResult = {}
+        expectedRotations = ''
+        expectResult['status'] = 'ok'
+
+        actualResult = solve.solve(parms)
+        self.assertEqual(expectedRotations, actualResult.get('solution'))
+        self.assertEqual(expectResult.get('status'), actualResult.get('status'))
+        
     # Sad Path
     def test_bottomCross_910_ErrorOnEmptyString(self):
         parms = {}
