faaa671eec3c211d68858350f2a4fc0cdade6743
Jonathan <jhs0051@auburn.edu>
2023-02-27 20:00:30 -0600

    Red Light      |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -3,4 +3,10 @@ from rubik.view.solve import solve
  
 
 class SolveTest(TestCase):
-    pass
+    # Sad Path
+    def test900_solve_ErrorOnShortCube(self):
+        parms = {}
+        parms['cube'] = 'w5i1w5i1S1iwa5iaSSSi1aa5iw15wSSia5waw1i5'
+        result = solve(parms)
+        self.assertIn('status', result)
+        self.assertEqual('error: invalid cube', result['status'])
