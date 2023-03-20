8bf4a269171298aa358012f2168eb04ccafb689b
Jonathan <jhs0051@auburn.edu>
2023-03-19 22:21:38 -0500

    Green Light    |

--- rubik/controller/middleLayer.py
+++ rubik/controller/middleLayer.py
@@ -1,4 +1,5 @@
 from rubik.model.constants import *
+from rubik.view.rotate import _rotate
 
 def _solveMiddleLayer(theCube, solution) -> str:
     if _isMiddleLayerSolved(theCube):
@@ -26,4 +27,32 @@ def _isMiddleLayerSolved(theCube):
     return True
 
 def _rotateMiddlePieceFromTopToMiddle(theCube, currentCubeIndex):
-    pass
+    cube = 'cube'
+    direction = 'dir'
+    directionList = ''
+
+    if currentCubeIndex is LTM:
+        if theCube[UML] is theCube[BMM]:
+            directionList += 'ubUBULul'
+        else:
+            directionList += 'UFufulUL'
+    elif currentCubeIndex is RTM:
+        if theCube[UMR] is theCube[FMM]:
+            directionList += 'ufUFURur'
+        else: 
+            directionList += 'UBuburUR'
+    elif currentCubeIndex is BTM:
+        if theCube[UTM] is theCube[RMM]:
+            directionList += 'urURUBub'
+        else:
+            directionList += 'ULulubUB'
+    elif currentCubeIndex is FTM:
+        if theCube[UBM] is theCube[LMM]:
+            directionList += 'ulULUFuf'
+        else:
+            directionList += 'URurufUF'
+
+    theCube = {cube: theCube, direction: directionList}
+    theCube = _rotate(theCube)[cube]
+
+    return theCube, directionList    
