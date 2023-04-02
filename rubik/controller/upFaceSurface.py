52069b467e528b6718d5731d945e5c3f8b726689
Jonathan <jhs0051@auburn.edu>
2023-04-02 18:34:29 -0500

    Green Light    |

--- rubik/controller/upFaceSurface.py
+++ rubik/controller/upFaceSurface.py
@@ -1,12 +1,12 @@
-import rubik.model.constants
-from rubik.model.cube import Cube
+from rubik.model.constants import *
+from rubik.view.rotate import rotate
 
-def solveUpSurface(theCube: Cube) -> str:
-    '''
-        This is the top-level function  for rotating
-        a cube so that the up face is solved.
-        
-        input:  an instance of the cube class with up-face cross solved
-        output: the rotations required to solve the up surface  
-    '''  
-    return 'U'      #TODO:  remove this stubbed value
+def solveUpSurface(theCube, solution) -> str:
+    if _isUpFaceSolved(theCube):
+        return theCube, solution
+    
+def _isUpFaceSolved(theCube):
+    topPieces = [theCube[UTL], theCube[UTM], theCube[UTR], theCube[UML], theCube[UMM], theCube[UMR],
+                 theCube[UBL], theCube[UBM], theCube[UBR]]
+
+    return all(piece == topPieces[FMM] for piece in topPieces)
