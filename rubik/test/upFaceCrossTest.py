c3b6cd896b32e9db396ac2ec98a5a2e6d95da51e
Jonathan <jhs0051@auburn.edu>
2023-04-02 15:04:51 -0500

    Yellow Light   |

---
+++ rubik/test/upFaceCrossTest.py
@@ -0,0 +1,25 @@
+'''
+Created on April 2, 2023
+
+@author: jonhoustonseibert
+'''
+import unittest
+import rubik.view.solve as solve
+from rubik.view.rotate import rotate
+from rubik.model.constants import *
+from rubik.controller import upFaceCross
+
+
+class UpFaceCrossTest(unittest.TestCase):
+
+    # Happy Path Tests  
+    def test_middleLayer_000_SolvedCubeShouldReturnOkStatus(self):
+        parms = {}
+        parms['cube'] = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
+        parms['dir'] = 'R'
+        
+        expectedResult = {}
+        expectedResult['status'] = 'ok'
+
+        actualResult = solve.solve(parms)
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
