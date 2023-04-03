061c32eb4ca58101f5579f5b7657ba55059fa659
Jonathan <jhs0051@auburn.edu>
2023-04-02 20:50:39 -0500

    Refactor       |

--- rubik/test/upFaceCrossTest.py
+++ rubik/test/upFaceCrossTest.py
@@ -11,7 +11,6 @@ from rubik.controller import upFaceCross
 
 
 class UpFaceCrossTest(unittest.TestCase):
-
     # Happy Path Tests  
     def test_upFaceCross_000_SolvedCubeShouldReturnOkStatus(self):
         parms = {}
@@ -45,11 +44,12 @@ class UpFaceCrossTest(unittest.TestCase):
         
     def test_upFaceCross_040_UpFaceDaisyProducesCorrectSolution(self):
         cube = 'rybggggggrgoooooooyoobbbbbbbyyrrrrrryygryygbywwwwwwwww'
+        rotations = ''
 
         expectedSolution = 'UUUFRUrufFRUruf'
         expectedCube = 'ybrggggggggyoooooobrgbbbbbboobrrrrrryyryyyoyywwwwwwwww'
 
-        actualCube, actualsolution = upFaceCross._makeUpFaceDaisy(cube, '')
+        actualCube, actualsolution = upFaceCross._makeUpFaceDaisy(cube, rotations)
 
         self.assertEqual(expectedCube, actualCube)
         self.assertEqual(expectedSolution, actualsolution)
@@ -76,25 +76,17 @@ class UpFaceCrossTest(unittest.TestCase):
             rotatedCube['dir'] = actualResult.get('solution')
             actualCube = rotate(rotatedCube).get('cube')
 
-            bottomCubeColors = actualCube[DTL] is actualCube[DTM] is actualCube[DTR] is actualCube[DML] is actualCube[
-                DMM] \
-                               is actualCube[DMR] is actualCube[DBL] is actualCube[DBM] is actualCube[DBR]
-            frontCubeColors = actualCube[FML] is actualCube[FMM] is actualCube[FMR] is actualCube[FBL] is actualCube[
-                FBM] \
-                              is actualCube[FBR]
-            rightCubeColors = actualCube[RML] is actualCube[RMM] is actualCube[RMR] is actualCube[RBL] is actualCube[
-                RBM] \
-                              is actualCube[RBR]
-            backCubeColors = actualCube[BML] is actualCube[BMM] is actualCube[BMR] is actualCube[BBL] is actualCube[BBM] \
-                             is actualCube[BBR]
-            leftCubeColors = actualCube[LML] is actualCube[LMM] is actualCube[LMR] is actualCube[LBL] is actualCube[LBM] \
-                             is actualCube[LBR]
-            upFaceCrossColors = actualCube[UTM] is actualCube[UML] is actualCube[UMM] is actualCube[UMR] is actualCube[UBM]
+            cubeFaces = {
+                "bottom": [DTL, DTM, DTR, DML, DMM, DMR, DBL, DBM, DBR],
+                "front": [FML, FMM, FMR, FBL, FBM, FBR],
+                "right": [RML, RMM, RMR, RBL, RBM, RBR],
+                "back": [BML, BMM, BMR, BBL, BBM, BBR],
+                "left": [LML, LMM, LMR, LBL, LBM, LBR],
+                "up": [UTM, UML, UMM, UMR, UBM],
+                }
 
-            self.assertTrue(bottomCubeColors)
-            self.assertTrue(frontCubeColors)
-            self.assertTrue(rightCubeColors)
-            self.assertTrue(backCubeColors)
-            self.assertTrue(leftCubeColors)
-            self.assertTrue(upFaceCrossColors)
+            for cubeFaces, cubeIndexes in cubeFaces.items():
+                faceColors = [actualCube[cubeIndex] for cubeIndex in cubeIndexes]
+                self.assertTrue(all(color is faceColors[FTL] for color in faceColors))
+
             self.assertEqual(expectResult.get('status'), actualResult.get('status'))
