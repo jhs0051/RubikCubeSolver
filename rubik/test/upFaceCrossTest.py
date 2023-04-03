e3ee01ed395f7efe54e0aafcfb30382e4d624571
Jonathan <jhs0051@auburn.edu>
2023-04-03 17:54:38 -0500

    Yellow Light   |

--- rubik/test/upFaceCrossTest.py
+++ rubik/test/upFaceCrossTest.py
@@ -66,7 +66,19 @@ class UpFaceCrossTest(unittest.TestCase):
         self.assertEqual(expectedCube, actualCube)
         self.assertEqual(expectedSolution, actualsolution)
         
-    def test_upFaceCross_050_SolveUpFaceCross(self):
+    def test_upFaceCross_050_UpFaceDaisyProducesCorrectSolution(self):
+        cube = 'rygbbbbbboybrrrrrryyyggggggryboooooogrobyoygywwwwwwwww'
+        rotations = ''
+
+        expectedSolution = 'FRUrufUUFRUrufFRUruf'
+        expectedCube = 'rbybbbbbboryrrrrrrgobggggggrgyooooooyyoyyygybwwwwwwwww'
+
+        actualCube, actualsolution = upFaceCross._makeUpFaceDaisy(cube, rotations)
+
+        self.assertEqual(expectedCube, actualCube)
+        self.assertEqual(expectedSolution, actualsolution)
+        
+    def test_upFaceCross_060_SolveUpFaceCross(self):
         cubes = ['orbyrrrrrrgoggggggyrroooooogyybbobbbyygbyybbywwwwwwwww',
                  'U2ig99iU9F9iUgFUiFggF92F92292F2FUgg2U99gUi2U2gFgiiFUii',
                  'kauEgkaEkEavvukggkkkgaauEEavuaakguvuugavEvEEgvuvgvuEkg',
