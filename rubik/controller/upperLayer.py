16e58024d066da77fd435a6397a3e26c7f3c2ec3
Jonathan <jhs0051@auburn.edu>
2023-04-15 17:29:05 -0500

    Green Light    |

--- rubik/controller/upperLayer.py
+++ rubik/controller/upperLayer.py
@@ -1,4 +1,5 @@
-import rubik.model.constants
+from rubik.model.constants import *
+from rubik.view.rotate import rotate
 from rubik.model.cube import Cube
 
 def solveUpperLayer(theCube: Cube) -> str:
@@ -11,5 +12,17 @@ def solveUpperLayer(theCube: Cube) -> str:
     '''  
     return 'D'      #TODO:  remove this stubbed value
 
-def _isTopLayerSolved(self):
-    pass
+def _isTopLayerSolved(theCube):
+    upFacePieces = [theCube[UTL], theCube[UTM], theCube[UTR], theCube[UML], theCube[UMM], theCube[UMR], theCube[UBL],
+                 theCube[UBM], theCube[UBR]]
+
+    topLayerPieces = [theCube[FTL], theCube[FTM], theCube[FTR], theCube[RTL], theCube[RTM], theCube[RTR], theCube[BTL],
+                  theCube[BTM], theCube[BTR], theCube[LTL], theCube[LTM], theCube[LTR]]
+
+    if upFacePieces.count(upFacePieces[FMM]) != len(upFacePieces) \
+            or topLayerPieces[FTL:FML].count(theCube[FMM]) != FML \
+            or topLayerPieces[FML:FBL].count(theCube[RMM]) != FML \
+            or topLayerPieces[FBL:RTL].count(theCube[BMM]) != FML \
+            or topLayerPieces[RTL:RML].count(theCube[LMM]) != FML:
+        return False
+    return True
