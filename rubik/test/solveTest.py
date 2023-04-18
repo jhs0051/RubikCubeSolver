dc12fa654fd81b44c3c446e0a3fa8c2aa434809b
Jonathan <jhs0051@auburn.edu>
2023-04-17 23:24:23 -0500

    Refactor       |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -291,35 +291,45 @@ class SolveTest(TestCase):
     def test_900_solve_ErrorOnShortCube(self):
         parms = {}
         parms['cube'] = 'w5i1w5i1S1iwa5iaSSSi1aa5iw15wSSia5waw1i5'
+        
         result = solve(parms)
+        
         self.assertIn('status', result)
         self.assertEqual('error: invalid cube', result['status'])
         
     def test_910_solve_ErrorOnLongCube(self):
         parms = {}
         parms['cube'] = 'PFUPFPLrL2rFUrrFPrrLL2PL22UUULrU222FPU2F2FrLPUPrFLLFUPPFUPFPLrL2rFUrrFPrrLL2PL22UUULrU222FPU2F2FrLPU'
+        
         result = solve(parms)
+        
         self.assertIn('status', result)
         self.assertEqual('error: invalid cube', result['status'])
         
     def test_920_solve_ErrorOnCubeWithIllegalCharacters(self):
         parms = {}
         parms['cube'] = 'bbbbbbbbb*********rrrrrrrrroooooooooyyyyyyyyywwwwwwwww'
+        
         result = solve(parms)
+        
         self.assertIn('status', result)
         self.assertEqual('error: invalid cube', result['status'])
         
     def test_930_solve_ErrorOnMoreThan9OfAColor(self):
         parms = {}
         parms['cube'] = 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwwb'
+        
         result = solve(parms)
+        
         self.assertIn('status', result)
         self.assertEqual('error: invalid cube', result['status'])
         
     def test_940_solve_ErrorOnNonUniqueMiddleCharacter(self):
         parms = {}
         parms['cube'] = 'rbbbbbbbbrrrrbrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'
+        
         result = solve(parms)
+        
         self.assertIn('status', result)
         self.assertEqual('error: invalid cube', result['status'])
         
@@ -327,7 +337,9 @@ class SolveTest(TestCase):
         parms = {}
         parms['cube'] = 'ogwwrywybgyrgbgrrwoogbgwyrworyryggwbbbyyowgobroobwoybr'
         parms['extra'] = 'key'
+        
         result = solve(parms)
+        
         self.assertIn('status', result)
         self.assertEqual('error: invalid key', result['status'])
         
