c2703271da524e79acc622ae5f222c7674cb9ead
Jonathan <jhs0051@auburn.edu>
2023-04-17 22:25:12 -0500

    Yellow Light   |

--- rubik/test/upFaceSurfaceTest.py
+++ rubik/test/upFaceSurfaceTest.py
@@ -138,4 +138,56 @@ class UpFaceSurfaceTest(unittest.TestCase):
                 self.assertTrue(all(color is faceColors[FTL] for color in faceColors))
 
             self.assertEqual(expectResult.get('status'), actualResult.get('status'))
+            
+    def test070_unsolvedCubesFromIteration5AreFullySolved(self):
+        cubes = ['bcjj9jc9PbbjPPPjjP9JJ9cJcc9PPJbbbJJbc9bcJc9JcJb9Pj9Pjj',
+                 'Odd66V3ww336w3ddOdw663OO3VOVdV3ww3wVwVd6V6wO66OOddVV3O',
+                 'DgiDff33333iiHigHiDfHDg3DgDffgfigHig3DgHDHH3fHiig3DfHf',
+                 'YBYbbKBxhKhKBxYbhYBKbxKbxbYBBbhhBKbKxYhhYYhxhxKBYBKxxb',
+                 '1Y1zGGzaaaaazzYYnazYzGaYzGnYGYa1a1nGGznnYnG1nn11zn1G1Y',
+                 'dPNfdttffffN55Pt55dtPNPPNd5ffttfNPN5tdfPtt55PPdddNNN5d',
+                 'uG3Uuu3YYUG8333uU3YYG888uU8YGUGG8Guu833YYUGu8U8GYUuU3Y',
+                 'O5rFor5rgooForO5gOO5rgOogOFO55Fgggro5rrg55gOFrFoOFooFF',
+                 '45G44D344EDDG55G45433DEE3EDEE5DGG5E4GGGG34D3D5333D5E5E',
+                 'sjssjjbbjKSKKKbSjbsKSSQQQSSKsjKbjKbQjsjQsQbSQSsbQSbQKs',
+                 'aJ0LJJa0MMaMMqMLLLLa00aaqa0q000MMMLLJMaJLqqqaqqJq0JJLJ',
+                 'GEPETFPFFFGnPnPnnPETPGFPFEnGFTTPPFTTTnTnEFnnGEEEGGGGTE',
+                 'sjjJPPJsPPxxJjAAjsPsJPsjxAjPsJJAAxAAAxjJxPjPJsjxsJxsxA',
+                 'bggeIIeII7Ib77s77sIgg7gg7bgssIsbbsbb77sbsseeegeegeebII',
+                 'weTS33SeEewwwSETSEe33TTeS3SwwewwT3S3EETEeT3TSESwEE3eeT',
+                 'M11BMM18Lk8kB1BBLM8k8L8BBMB1118BkLk8L1B8kkLLMkLMMLM81k',
+                 'RskRRUs6UU62s22sksRR2UU6RUUU2kRss6kRkk6Rks2ksk2626U266',
+                 'yoTyTTlcOOlccOycyOTlOTooycocyTOlOlTyoTloycllyoOTOclcoo',
+                 'NTGGlNTlG444444AAAllGTTGlNlAl4AATGANlNNGGGTNTA4TlNT4AN',
+                 'llKllkklleexxxKJkKJkKxkkxKkkxxJexeKKeKkJKJJelxJeeJeJll'
+                ]
+        for cube in cubes:
+            parms = {}
+            parms['op'] = 'solve'
+            parms['cube'] = cube
+
+            expectResult = {}
+            expectResult['status'] = 'ok'
+
+            actualResult = solve.solve(parms)
+
+            rotatedCube = {}
+            rotatedCube['cube'] = parms.get('cube')
+            rotatedCube['dir'] = actualResult.get('solution')
+            actualCube = rotate(rotatedCube).get('cube')
+
+            cubeFaces = {
+                "bottom": [DTL, DTM, DTR, DML, DMM, DMR, DBL, DBM, DBR],
+                "front": [FTL, FTM, FTR, FML, FMM, FMR, FBL, FBM, FBR],
+                "right": [RTL, RTM, RTR, RML, RMM, RMR, RBL, RBM, RBR],
+                "back": [BTL, BTM, BTR, BML, BMM, BMR, BBL, BBM, BBR],
+                "left": [LTL, LTM, LTR, LML, LMM, LMR, LBL, LBM, LBR],
+                "up": [UTL, UTM, UTR, UML, UMM, UMR, UBL, UBM, UBR],
+            }
+
+            for cubeFaces, cubeIndexes in cubeFaces.items():
+                faceColors = [actualCube[cubeIndex] for cubeIndex in cubeIndexes]
+                self.assertTrue(all(color is faceColors[FTL] for color in faceColors))
+
+            self.assertEqual(expectResult.get('status'), actualResult.get('status'))
         
